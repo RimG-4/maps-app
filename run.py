99074d83e6341e6c9ebc1126ad3f99071d1377bd
Rimma <lemonsensei04@gmail.com>
2025-04-14 22:12:04 +0500

    Подключение к PostgreSQL на Render

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 from app import create_app
+from dotenv import load_dotenv
 
+# Загружаем переменные окружения из файла .env
+load_dotenv()
+
+# Создаём экземпляр приложения
 app = create_app()
 
 if __name__ == "__main__":
+    # Запускаем приложение в режиме отладки
     app.run(debug=True)
