f9f5e19581331303d6564ee08371389a85fdd49d
Rimma <lemonsensei04@gmail.com>
2025-04-15 16:54:08 +0500

    добавили логи2

--- app/__init__.py
+++ app/__init__.py
@@ -1,45 +1,38 @@
 from flask import Flask, redirect, url_for
-from app.controllers.navigation_controller import navigation_bp
 from flask_migrate import Migrate
+from flask_login import LoginManager, current_user
 import os
 import sys
-from app.utils import config
-from app.utils.config import DATABASE_URL
-from app.models import *
 from app.utils.db import db
-from flask_login import current_user
-from flask_login import LoginManager
-
+from app.utils.config import Config
+from app.models import T_User
+from app.controllers.navigation_controller import navigation_bp
+from app.controllers.auth_controller import auth_bp
 
 migrate = Migrate()
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
 
-    app.config.from_object(config)
-    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.secret_key = os.environ.get('SECRET_KEY', 'default-secret')
+    # Настройки из config.py
+    app.config.from_object(Config)
 
+    # Инициализация расширений
     db.init_app(app)
-    Migrate(app, db)
-
-    # Регистрация блюпринтов
-    from app.controllers.auth_controller import auth_bp
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(navigation_bp, url_prefix='/navigation')
+    migrate.init_app(app, db)
 
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
+
+    # Регистрация блюпринтов
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(navigation_bp, url_prefix='/navigation')
 
     @login_manager.user_loader
     def load_user(user_id):
         return T_User.query.get(int(user_id))
 
-    # Роут для главной страницы
     @app.route('/')
     def index():
         if current_user.is_authenticated:
--- app/utils/config.py
+++ app/utils/config.py
@@ -1,2 +1,6 @@
 import os
-DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:Max.40WSQL@localhost/maps")
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev_secret_key')
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "postgresql://postgres:Max.40WSQL@localhost/maps")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
